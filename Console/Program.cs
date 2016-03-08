b99039b50c210f6ae47f843ade3d71c16b37d404
吳剛志 <chicken@chicken-house.net>
2016-03-09 01:24:38 +0800

    add: time test project

--- Console/Program.cs
+++ Console/Program.cs
@@ -12,43 +12,119 @@ namespace Console
     {
         public static void Main(string[] args)
         {
-            int total_threads = 10;
+            int total_threads = 8;
             for (int i = 0; i < total_threads; i++)
             {
-                Thread t = new Thread(Program.DrawSinWave);
+                Thread t = new Thread(Program.DrawBitmap);
                 t.Start();
             }
-
-            //DrawSinWave();
         }
 
-        public static void DrawSinWave()
-        {
-            long unit = 100;
 
+        public static string[] bitmap = new string[]
+        {
+            @"              XXXX          X      X          XXXX              ",
+            @"             X   X          XX    XX          X   X             ",
+            @"            X    X         X  XXXX  X         X    X            ",
+            @"           X      XXXX     X        X     XXXX      X           ",
+            @"          X           XXXXX          XXXXX           X          ",
+            @"         X                                            X         ",
+            @"        X                                              X        ",
+            @"       X                                                X       ",
+            @"      X                                                  X      ",
+            @"     X                                                    X     "
+        };
+        //public static string[] bitmap = new string[]
+        //{
+        //    @"                 XXXX                ",
+        //    @"                 X   X               ",
+        //    @"                X    X               ",
+        //    @"      XXXXXXXXXX     XXXXXXX         ",
+        //    @"      X     X X             X        ",
+        //    @"      X     X X              X       ",
+        //    @"      X     X X              X       ",
+        //    @"      X     X X              X       ",
+        //    @"      X     X X              X       ",
+        //    @"      XXXXXXX XXXXXXXXXXXXXXXX       "
+        //};
+
+
+
+
+        public static long[] GetDataFromBitmap(long period, long unit)
+        {
+            int max_x = bitmap[0].Length;
+            int max_y = bitmap.Length;
+            long steps = period / unit;
+
+            long[] data = new long[steps];
+
+            for (int s = 0; s < steps; s++)
+            {
+                int x = (int)(s * max_x / steps);
+                int value = 0;
+                for (int y = 0; y < bitmap.Length; y++)
+                {
+                    value = y;
+                    if (bitmap[y][x] == 'X') break;
+                }
+                data[s] = value * unit / max_y;
+            }
+
+            return data;
+        }
+
+        public static long[] GetDataFromSineWave(long period, long unit)
+        {
+            long steps = period / unit;
+            long[] data = new long[steps];
+
+            for (int s = 0; s < steps; s++)
+            {
+                //long degree = s * 360 / steps
+                data[s] = (long)(unit - (Math.Sin(Math.PI * s * 360 / steps / 180.0) / 2 + 0.5) * unit);
+            }
+
+            return data;
+        }
+
+        public static void DrawBitmap()
+        {
+            long unit = 100; // ms
+            long period = 60 * 1000; // msec, full image time period
+
+            long[] data = GetDataFromBitmap(period, unit);
+            //long[] data = GetDataFromSineWave(period, unit);
 
             Stopwatch timer = new Stopwatch();
             timer.Restart();
             while (true)
             {
-                //SpinWait.SpinUntil(
-                //    () => { return false; },
-                //    (int)(unit - timer.ElapsedMilliseconds % unit));
-                //Thread.Sleep((int)(unit - timer.ElapsedMilliseconds % unit));
-
-
-
-
-                long degree = (timer.ElapsedMilliseconds / unit) % 360;
-
+                //long degree = (timer.ElapsedMilliseconds / unit) % 360;
+                long step = (timer.ElapsedMilliseconds / unit) % (period / unit);
                 long offset = (long)(timer.ElapsedMilliseconds % unit);
-
-                long v = (long)(unit - (Math.Sin(Math.PI * degree / 180.0) / 2 + 0.5) * unit);
+                long v = data[step];//(long)(unit - (Math.Sin(Math.PI * degree / 180.0) / 2 + 0.5) * unit);
                 long idle_until = timer.ElapsedMilliseconds - offset + v;
                 long busy_until = timer.ElapsedMilliseconds - offset + unit;
 
-                //long offset = (timer.ElapsedMilliseconds % unit);
-                //Console.WriteLine("{0},{1}", degree, v);
+                SpinWait.SpinUntil(() => { return (timer.ElapsedMilliseconds > idle_until); });
+                while (timer.ElapsedMilliseconds < busy_until) ;
+            }
+        }
+
+
+        public static void DrawSinWave()
+        {
+            long unit = 100;
+            Stopwatch timer = new Stopwatch();
+            timer.Restart();
+            while (true)
+            {
+                long degree = (timer.ElapsedMilliseconds / unit) % 360;
+                long offset = (long)(timer.ElapsedMilliseconds % unit);
+                long v = (long)(unit - (Math.Sin(Math.PI * degree / 180.0) / 2 + 0.5) * unit);
+                long idle_until = timer.ElapsedMilliseconds - offset + v;
+                long busy_until = timer.ElapsedMilliseconds - offset + unit;
 
                 SpinWait.SpinUntil(() => { return (timer.ElapsedMilliseconds > idle_until); });
                 while (timer.ElapsedMilliseconds < busy_until) ;
---
+++ TimerTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimerTest
+{
+    class Program
+    {
+        static bool stop = false;
+
+        static void Main(string[] args)
+        {
+            // noise
+            {
+                for (int i = 0; i < 0; i++)
+                {
+                    Thread t = new Thread(Foo);
+                    //t.Priority = ThreadPriority.BelowNormal;
+                    t.Start();
+                }
+
+            }
+
+
+            Stopwatch timer = new Stopwatch();
+            TimeSpan idle = TimeSpan.FromMilliseconds(30);
+
+            // test Thread.Sleep()
+            for(int i = 0; i < 10; i++)
+            {
+                timer.Restart();
+                Thread.Sleep(idle);
+                Console.WriteLine("Sleep(10): take {0} ms", timer.ElapsedMilliseconds);
+            }
+
+            // test SpinWait.SpinUntil()
+            for(int i = 0; i<10; i++)
+            {
+                timer.Restart();
+                SpinWait.SpinUntil(() => { return false; }, idle);
+                Console.WriteLine("SpinUntil(10): take {0} ms", timer.ElapsedMilliseconds);
+            }
+
+            stop = true;
+        }
+
+        static Random _rnd = new Random();
+
+        static void Foo()
+        {
+            while (stop == false) if (_rnd.Next() % 2 == 0) Thread.Sleep(0);
+        }
+    }
+}
